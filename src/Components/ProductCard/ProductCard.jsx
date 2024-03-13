40170bb82a1dad78f5e7fd02feb4d6b53f8d74cf
ShenalGanegoda <106686816+ShenalGanegoda@users.noreply.github.com>
2024-03-13 15:44:20 +0530

    Retrive product completed

--- src/Components/ProductCard/ProductCard.jsx
+++ src/Components/ProductCard/ProductCard.jsx
@@ -9,10 +9,12 @@ export default function ProductCard({ product, setProduct }) {
     <div className="product-card-container">
       <img src={product.image} alt="Product Image"></img>
       <div>
-        <h2>{product.name}</h2>
+        <h2>{product.productName}</h2>
       </div>
       <div className="product-card-buttons">
-        <button className='view-btn' onClick={() => handleViewProduct()}>View Analysis</button>
+        <button className="view-btn" onClick={() => handleViewProduct()}>
+          View Analysis
+        </button>
         <button className="remove-btn">Remove</button>
       </div>
     </div>
