28d81cf6f73f224c481fa55008fc7a367aa534a4
Uvindujayawee <uvindu.20220143@iit.ac.lk>
2024-03-18 22:46:10 +0530

    method implemented to save product image

--- src/Pages/CustomerReview.jsx
+++ src/Pages/CustomerReview.jsx
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-
+import './CustomerReview.css';
 export default function CustomerReview() {
+  
   const { employee, foodItems } = useParams();
-
   const foodArray = foodItems.split(",");
+  const [displayHeaderAndNavbar, setDisplayHeaderAndNavbar] = useState (true);
+  
 
+  
+
+  useEffect (() => {
+    
+  console.log('useEffect is running');
+    const params = new URLSearchParams(window.location.search);
+    const fromQR = params.get('fromQR');
+    console.log('fromQR',fromQR);
+    if (fromQR === 'true') {
+      console.log('Set displayHeaderAndNavbar to false')
+      setDisplayHeaderAndNavbar(false);
+    }
+  },[])
+
+  console.log('displayHeaderAndNavbar',displayHeaderAndNavbar); 
+  
   return (
-    <div>
+    <div className="employee_food">
       <h1>Customer Review</h1>
       <h2>{employee}</h2>
       <h2>{foodItems}</h2>
@@ -18,7 +36,7 @@ export default function CustomerReview() {
       <hr></hr>
       {/* Dynamically render input fields for ordered food */}
       {foodArray.map((food) => (
-        <div>
+        <div className="food">
           <label>{food}</label>
           <input placeholder="Add Comment"></input>
         </div>
--- src/Pages/Products.jsx
+++ src/Pages/Products.jsx
@@ -1,10 +1,32 @@
 import ProductList from "../Components/ProductList/ProductList";
 import ViewProduct from "../Components/ViewProduct/ViewProduct";
 import { useState } from "react";
+import axios from "axios";
 
 export default function Products() {
   const [showProductDetails, setshowProductDetails] = useState(false);
   const [selectedProduct, setselectedProduct] = useState({});
+  const [imageFile, setImageFile] =useState (null);
+
+  //method to handle product image
+  const retrieveAndSaveImage = async (event) => {
+    const file = event.target.files[0];
+    setImageFile(file);
+  
+  //Image upload to backend
+  try{
+    const dataForm = new dataForm();
+    dataForm.append("image",file);
+  //Post request to backend API
+    const reqBackend = await axios.post("/API/uploadImage",dataForm,{
+      headers:{
+        "Content-type": "multipart/form-data"
+      }
+    });
+    console.log("Image Uploaded Successfully!:",reqBackend.data);
+  }catch (error){
+    console.log("Upload failed",error)
+  }
 
   const setProduct = (product) => {
     setselectedProduct(product);
@@ -18,8 +40,10 @@ export default function Products() {
       ) : (
         <ProductList setProduct={setProduct} />
       )}
-
+      {/*Input for Image upload*/}
+      <input type="file" onChange={retrieveAndSaveImage}/>
       {/* <Outlet/> */}
     </div>
   );
+ }
 }
