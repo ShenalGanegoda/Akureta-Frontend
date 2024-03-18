3f60c23d7cf95d0487e3ca8abd9f885344151ed4
Uvindujayawee <uvindu.20220143@iit.ac.lk>
2024-03-18 12:28:19 +0530

    Changes done in CustomerReview function

--- src/Pages/CustomerReview.jsx
+++ src/Pages/CustomerReview.jsx
@@ -2,21 +2,20 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import './CustomerReview.css';
 export default function CustomerReview() {
+  
   const { employee, foodItems } = useParams();
-
+  const foodArray = foodItems.split(",");
   const [displayHeaderAndNavbar, setDisplayHeaderAndNavbar] = useState (true);
 
   useEffect (() => {
 
     const params = new URLSearchParams(window.location.search);
-    const fromQRCode = params.get('fromQRCode');
-    if (fromQRCode === 'true') {
+    const fromQR = params.get('fromQR');
+    if (fromQR === 'true') {
       setDisplayHeaderAndNavbar(false);
     }
   },[])
-
-  const foodArray = foodItems.split(",");
-  
+  console.log('displayHeaderAndNavbar',displayHeaderAndNavbar); 
   
   return (
     <div className="employee_food">
