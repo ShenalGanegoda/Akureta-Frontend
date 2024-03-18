33294d036c4bd7124defdb077423351c9bcfdde2
Uvindujayawee <uvindu.20220143@iit.ac.lk>
2024-03-18 12:18:09 +0530

    Header and navbar css removed

--- src/Pages/CustomerReview.jsx
+++ src/Pages/CustomerReview.jsx
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-
+import './CustomerReview.css';
 export default function CustomerReview() {
   const { employee, foodItems } = useParams();
 
+  const [displayHeaderAndNavbar, setDisplayHeaderAndNavbar] = useState (true);
+
+  useEffect (() => {
+
+    const params = new URLSearchParams(window.location.search);
+    const fromQRCode = params.get('fromQRCode');
+    if (fromQRCode === 'true') {
+      setDisplayHeaderAndNavbar(false);
+    }
+  },[])
+
   const foodArray = foodItems.split(",");
   
+  
   return (
-    <div>
+    <div className="employee_food">
       <h1>Customer Review</h1>
       <h2>{employee}</h2>
       <h2>{foodItems}</h2>
@@ -18,7 +30,7 @@ export default function CustomerReview() {
       <hr></hr>
       {/* Dynamically render input fields for ordered food */}
       {foodArray.map((food) => (
-        <div>
+        <div className="food">
           <label>{food}</label>
           <input placeholder="Add Comment"></input>
         </div>
