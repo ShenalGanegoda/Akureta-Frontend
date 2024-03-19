372b48f1c6cdcb97972bc12f09316d48a87f8023
ShenalGanegoda <106686816+ShenalGanegoda@users.noreply.github.com>
2024-03-19 14:39:46 +0530

    Products and Employees missing error fixed

--- src/Pages/Employees.jsx
+++ src/Pages/Employees.jsx
@@ -4,47 +4,48 @@ import { useState } from "react";
 import axios from "axios";
 
 export default function Employees() {
-  const [showEmployeeDetails, setshowEmployeeDetails] = useState(false);
-  const [selectedEmployee, setselectedEmployee] = useState({});
-  const [imageFile, setImageFile] =useState (null);
+  const [showEmployeeDetails, setShowEmployeeDetails] = useState(false);
+  const [selectedEmployee, setSelectedEmployee] = useState({});
+  const [imageFile, setImageFile] = useState(null);
 
+  // Method to set selected employee
   const setEmployee = (employee) => {
-    setselectedEmployee(employee);
-    setshowEmployeeDetails(true);
+    setSelectedEmployee(employee);
+    setShowEmployeeDetails(true);
   };
-  //method to handle employee image
+
+  // Method to handle employee image
   const retrieveAndSaveImage = async (event) => {
     const file = event.target.files[0];
     setImageFile(file);
-  
-  //Image upload to backend
-  try{
-    const dataForm = new dataForm();
-    dataForm.append("image",file);
-  //Post request to backend API
-    const reqBackend = await axios.post("/API/uploadImage",dataForm,{
-      headers:{
-        "Content-type": "multipart/form-data"
-      }
-    });
-    console.log("Image Uploaded Successfully!:",reqBackend.data);
-  }catch (error){
-    console.log("Upload failed",error)
-  }
+
+    // Image upload to backend
+    try {
+      const dataForm = new FormData();
+      dataForm.append("image", file);
+      // Post request to backend API
+      const reqBackend = await axios.post("/API/uploadImage", dataForm, {
+        headers: {
+          "Content-type": "multipart/form-data",
+        },
+      });
+      console.log("Image Uploaded Successfully!:", reqBackend.data);
+    } catch (error) {
+      console.log("Upload failed", error);
+    }
+  };
+
   return (
     <div className="test">
-      {/* <h1>Employees {selectedEmployee ? `> ${selectedEmployee.name}` : ""}</h1> */}
       <h1>Employees</h1>
-
       {showEmployeeDetails ? (
         <ViewEmployee selectedEmployee={selectedEmployee} />
       ) : (
         <EmployeeList setEmployee={setEmployee} />
       )}
-      {/*Input for Image upload*/}
-      <input type="file" onChange={retrieveAndSaveImage}/>
+      {/* Input for Image upload */}
+      <input type="file" onChange={retrieveAndSaveImage} />
       {/* <Outlet/> */}
     </div>
   );
- }
 }
